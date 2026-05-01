fb9fbd4ea712e09febd1be1598469ffe366279c0
Soilihi Mohamed <amirmaoueni@gmail.com>
2026-05-01 01:05:10 +0000

    fix(http): correction de la configuration HttpClient avec interceptors et fetch

--- stock-manager-client/src/app/app.config.ts
+++ stock-manager-client/src/app/app.config.ts
@@ -2,21 +2,24 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http'; // Importez withFetch
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+
 import { BASE_PATH } from './api';
 import { authInterceptor } from './auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), 
-    
+    provideRouter(routes),
+
     provideClientHydration(withEventReplay()),
-    provideHttpClient(withInterceptors([authInterceptor])),
-    
-    // 1. Ajout de withFetch() pour supprimer l'erreur NG02801
-    provideHttpClient(withFetch( )), 
-    
-    { provide: BASE_PATH, useValue:'http://localhost:8080' }
+
+    // ✅ UN SEUL HttpClient propre
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([authInterceptor])
+    ),
+
+    { provide: BASE_PATH, useValue: 'http://localhost:8080' }
   ]
 };
